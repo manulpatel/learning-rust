74b8de2ea0d7727c5e947e3fc0fdd08da039c1a4
Manul Patel <manulpatel99@gmail.com>
2023-04-26 23:37:49 +0530

    Added Notes

--- main.rs
+++ main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello World without Cargo!");
 }
+
+//NOTES
+
+//Variables
+//let,const,static (Use mut to make mutable)
+//Rust is statically typed;So need to explicilty decalre type of varible except using 'let'.
+
+//Naming Convention
+//{Variable,Function,Files}=snake_case
+//{Constants, Statics}=SCREAMING_SNAKE_CASE
+//{Type,Traits,Enums}=PascalCase
+
