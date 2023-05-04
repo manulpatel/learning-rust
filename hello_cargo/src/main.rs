e2d2905d7c64775c7abf54dc6981636352d54af0
Manul Patel <manulpatel99@gmail.com>
2023-05-04 22:36:35 +0530

    Constants and Shadowing

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -9,3 +9,19 @@ fn main() {
 //.\target\debug\hello_cargo.exe
 //cargo run //to comibe above 2 steps
 //cargo check //to check for build errors, not product .exe
+
+
+
+//Constants
+//const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+ 
+//Shadowing
+//let x = 5;
+//let x = x + 1; //let is important
+//let spaces = "   ";
+//let spaces = spaces.len(); //change in type
+
+
+
+
+
